3f7f1f5891df37f9f2e4ebe6bb21b460023ca0ff
devanshu-bisht23 <devanshubisht23@gmail.com>
2026-03-14 11:41:25 +0530

    layout.tsx changes

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,15 +1,35 @@
+// import { Outlet } from "react-router-dom"
+// import Sidebar from "../components/Sidebar"
+// import BottomNav from "../components/BottomNav"
+
+
+// const Layout = () => {
+//   return (
+//     <div className="layout-container">
+//         <Sidebar />
+//         <div className="flex-1 overflow-y-scroll">
+//           <Outlet />
+//         </div>
+//       <BottomNav />
+//     </div>
+//   )
+// }
+
+// export default Layout
+
 import { Outlet } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
 import BottomNav from "../components/BottomNav"
 
-
 const Layout = () => {
   return (
     <div className="layout-container">
-        <Sidebar />
-        <div className="flex-1 overflow-y-scroll">
-          <Outlet />
-        </div>
+      <Sidebar />
+
+      <div className="flex-1 overflow-y-auto pb-20">
+        <Outlet />
+      </div>
+
       <BottomNav />
     </div>
   )
