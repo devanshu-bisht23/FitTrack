6765a66b4fff89b5cb01ddda12a30705f74c14c8
devanshu-bisht23 <devanshubisht23@gmail.com>
2026-03-14 11:32:14 +0530

    Added live step for mobile

--- client/src/components/BottomNav.tsx
+++ client/src/components/BottomNav.tsx
@@ -1,4 +1,4 @@
-import { Activity, Home, User, Utensils } from "lucide-react"
+import { Activity, Home, User, Utensils, Footprints  } from "lucide-react"
 import { NavLink } from "react-router-dom"
 
 const BottomNav = () => {
@@ -7,6 +7,7 @@ const BottomNav = () => {
         {path: '/', label: 'Home', icon:Home},
         {path: '/food', label: 'Food', icon: Utensils},
         {path: '/activity', label: 'Activity', icon: Activity},
+        { name: "Live Steps", icon: Footprints, path: "/steps" },
         {path: '/profile', label: 'Profile', icon: User},
     ]
 
